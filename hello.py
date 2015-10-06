47da1b8307d23f5817967bfb2c8958f7528293cc
Thuy Linh NGUYEN <nguyenthuylinh1986@gmail.com>
2015-10-06 19:15:35 +0200

    create a web site

---
+++ hello.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*
+
+import cgi 
+
+form = cgi.FieldStorage()
+print("Content-type: text/html; charset=utf-8\n")
+
+html = """<!DOCTYPE html>
+<head>
+    <title>Bonjour</title>
+</head>
+<body>
+	<!DOCTYPE html>
+<body>
+	<i><b><h1> Bienvenue sur mon site </h1></b></i>
+<form method="POST" action="/formulaire.py">
+<legend>Inscription</legend>
+<input type="text" name="nom" placeholder="Nom"  />
+<br />
+<input type="text" name="prenom" placeholder="Prenom"  />
+<br />
+<input type="text" password="mot de passe" placeholder="Mot de passe" />
+<br />
+<input type="submit" class="btn btn-primary" value="Valider" />
+</form>
+</body>
+</html>
+</body>
+</html>
+"""
+
+print(html)
